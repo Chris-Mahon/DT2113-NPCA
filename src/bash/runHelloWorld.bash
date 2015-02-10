fd8f66729618655656387e9dc9be19dc01662b1f
markdeegan <mark@dit.ie>
2015-02-10 09:58:19 +0000

    Adding Hello World.c and two bash scripts to build and run it

---
+++ src/bash/runHelloWorld.bash
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Mark Deegan
+# Tue 10 Feb 2015 09:53:42 GMT
+# DT211-3
+# Network Programming
+# February 2015
+# CA-2
+#
+# script to run HelloWorld.o
+# this script should be run from the src/bash directory of the project
+
+# this runs HelloWorld.o from the bin directory
+../../bin/HelloWorld.o
