b45083293e6038ced962d31d5d836b65d9fd8e71
Christopher Mahon <christopher.mahon2@student.dit.ie>
2015-02-26 19:09:45 +0000

    Added template to the java files

---
+++ src/java/SimpleJavaClient.java
@@ -0,0 +1,17 @@
+/** all code should be developed as part of a package to ensure namespace
+separation between many developers developing their own versions of utilities, classes and functions */
+package ie.dit.student.mahon.christopher;
+
+/** Christopher Mahon
+Simple Client to connect to a server */
+public class SimpleJavaClient
+{
+
+	/** start declaration of main method */
+	public static void main(String[] args)
+	{
+		// print the hello message
+		System.out.println("Client");
+	}// end declaration of main method
+
+} // end declaration of utility class
---
+++ src/java/SimpleJavaServer.java
@@ -0,0 +1,17 @@
+/** all code should be developed as part of a package to ensure namespace
+separation between many developers developing their own versions of utilities, classes and functions */
+package ie.dit.student.mahon.christopher;
+
+/** Christopher Mahon
+Simple server that would allow a client to connect to it */
+public class SimpleJavaServer
+{
+
+	/** start declaration of main method */
+	public static void main(String[] args)
+	{
+		// print the hello message
+		System.out.println("Server");
+	}// end declaration of main method
+
+} // end declaration of utility class
