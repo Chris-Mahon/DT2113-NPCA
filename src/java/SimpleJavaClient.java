c1d4879e8ab74028d6ad498a60e003dba28c42ab
Chris Mahon <chris-mahon@hotmail.com>
2015-03-02 02:29:47 +0200

    Commented Java Client

--- src/java/SimpleJavaClient.java
+++ src/java/SimpleJavaClient.java
@@ -1,9 +1,9 @@
-/** all code should be developed as part of a package to ensure namespace
-separation between many developers developing their own versions of utilities, classes and functions */
+
 package ie.dit.student.mahon.christopher;
 
 import java.net.*;
 import java.io.*;
+
 /** Christopher Mahon
 Simple Client to connect to a server */
 public class SimpleJavaClient
@@ -13,7 +13,7 @@ public class SimpleJavaClient
 	public static void main(String[] args)
 	{
 		Socket ClientSocket = null; //Creating a blank Socket
-		String OutMessage = "Heya";
+		String OutMessage = "Heya";//Message that outputs to the server, will be replaced with user input
 		//Main Function
 		try
 		{//Trying to connect to the server and pulling any data sent by the server to it
@@ -21,25 +21,30 @@ public class SimpleJavaClient
 		}
 		catch(IOException e)
 		{//If the socket fails, itll print out an error message
-			System.out.println("Nope Broken");
-			e.printStackTrace();
+			System.out.println("Nope Broken");//Error Message for the socket creating failing
+			e.printStackTrace();//Prints out an exact error message
 		} // end of catch to catch any sort of exception
 		
+		
+		//Somewhere the transmission from the server is sent to the server and recieved 
+		//but no response is printed out on this side
+		
 		try
-		{//Trying to connect to the server and pulling any data sent by the server to it
+		{//Sending a message and receiving a confirmation from the server
+			//Creates an output stream to  the server to which the program will write out a message
 			PrintWriter output = new PrintWriter(ClientSocket.getOutputStream(), true);
 			
+			//Creates an buffer for incoming messages
 			BufferedReader in = new BufferedReader(new InputStreamReader(ClientSocket.getInputStream()));
-			output.println(OutMessage);
-			
-			System.out.println("The response is:" + in.readLine());
+			output.println(OutMessage); //Printing to the Output stream to the server
+			String Incoming = in.readUTF(); //Reads in a string from the server
+			System.out.println("The response is:" + Incoming); //Prints the data from the server
 		}
 		catch(IOException e)
-		{//If the socket fails, itll print out an error message
+		{//If the message transfer fails, itll print out an error message
 			System.out.println("Nope doesnt work");
 			e.printStackTrace();
 		} // end of catch to catch any sort of exception
-
 
 	}// end declaration of main method
 
