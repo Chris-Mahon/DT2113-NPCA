ee8561976e0cf304a8d4c96b882b40017fd70e7c
Chris Mahon <chris-mahon@hotmail.com>
2015-03-05 02:10:12 +0200

    Adding more comments to C Client

--- src/bash/runJavaClient.bash
+++ src/bash/runJavaClient.bash
@@ -1,1 +1,1 @@
-java -cp ../../bin ie.dit.student.mahon.christopher.SimpleJavaClient localhost 4441 Hi
+java -cp ../../bin ie.dit.student.mahon.christopher.SimpleJavaClient localhost 4444 Hi
--- src/c/SimpleCClient.c
+++ src/c/SimpleCClient.c
@@ -12,27 +12,28 @@
 #include <unistd.h>
 
 void error(char *msg) 
-{
+{//Function that will print out an error message and close the program when called
 	perror(msg); // print the provided error message
     exit(0);     // exit the program with exit code 0
 }
 
 int main(int argc, char *argv[]) 
-{
-	int clientsock, portno;
+{//Main function
+	//Initialisation
+	int clientsock, portno; 
 	char buffer[256], hostaddr[24];
 	struct sockaddr_in host;	
 	int n, x;
 
-	if (argc <3) 
-	{
-		fprintf(stderr,"ERROR, the program should be called with the following parameters: \nProgram_Name hostname port");
+	if (argc !=3) 
+	{//Checking if there are an invalid number of parameters supplied
+		fprintf(stderr,"ERROR, the program should be called with the following parameters: \nProgram_Name hostname port\n");
 		exit(-1); 
-	}
+	}//End of check
 	
 	if (strcmp(argv[1], "localhost"))
-	{
-		strcpy(hostaddr, "127.0.0.1");
+	{//Converts localhost to a ip address relating to the local machine
+		strcpy(hostaddr, "127.0.0.1"); //Copies the ip version of localhost into a variable to be used later 
 		
 	}
 	else
@@ -44,11 +45,15 @@ int main(int argc, char *argv[])
 	printf("Enter in a message to send to the server");
 	scanf("%s", buffer);
 	
-    clientsock = socket(AF_INET, SOCK_STREAM, 0);
-    if (clientsock < 0) error("ERROR opening socket");
+    clientsock = socket(AF_INET, SOCK_STREAM, 0);//Creates an instance of the socket
+    if (clientsock < 0) error("ERROR opening socket"); //Checks if the socket creation succeeded
 
-    bzero((char *) &host, sizeof(host));
-    portno = atoi(argv[2]);
+    bzero((char *) &host, sizeof(host));//Initialises the variable host to zeros, so as to prevent random data from previous programs to effect the system
+    portno = atoi(argv[2]); //creates a local instance of the second parameter which is the port number
+	if (portno == 0)
+	{
+		error("Conversion of port failed");
+	}
 	
     host.sin_family = AF_INET;
     host.sin_port = htons(portno);
